68492b5361b42d015450745f83088c2f24d210ed
MaxBortolotti <115532971+MaxBortolotti@users.noreply.github.com>
2023-11-15 10:50:16 +0100

    Upload Max: nouvelle fct displayCellsInList
    Upload Max avec nouvelle fct displayCellsInList et fct addCellToHeadList revisitée par Roland

--- main.c
+++ main.c
@@ -6,10 +6,13 @@
 int main() {
     Cell* myCell = createCell(5,2);
     //DEBUGDisplayCell(myCell);
-    List* myList = createEmptyList(2);
-    addCellToHeadList(myList, 3, 1);
-    addCellToHeadList(myList, 6, 1);
-    displayCellsInListByLevel(myList,1);
+    List* myList = createEmptyList(3);
+    addCellToHeadList(myList, 3, 3);
+    addCellToHeadList(myList, 6, 2);
+    addCellToHeadList(myList, 7, 1);
+    //myList->cellsHeads.next[1] =  myList->cellsHeads.next[0]->cellsNext.next[0];
+    displayCellsInListByLevel(myList,2);
+    displayCellsInList(myList);
 
     return 0;
 }
