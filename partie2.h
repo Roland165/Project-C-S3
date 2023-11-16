5a68bbeb870549a07116bdb6d07a8c9f9cc2313d
MatALass <115532932+MatALass@users.noreply.github.com>
2023-11-16 10:22:44 +0100

    Create partie2.h

---
+++ partie2.h
@@ -0,0 +1,8 @@
+#ifndef UNTITLED13_PARTIE2_H
+#define UNTITLED13_PARTIE2_H
+#include "list.h"
+
+List* createListFromValue(int n);
+Cell* searchInTheLowestLevel(List* myList, int target);
+Cell* searchFromHighestLevel(List* myList, int target);
+#endif //UNTITLED13_PARTIE2_H
