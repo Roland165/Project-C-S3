f3e6c66419f293b501333566fad798add1442656
MaxBortolotti <115532971+MaxBortolotti@users.noreply.github.com>
2023-11-29 09:08:51 +0100

    Ajout de commentaires à fct createListFromValue(int n)
    Max a ajouté des commentaires pour la fonction 'createListFromValue(int n)', pour bien comprendre son fonctionnement et ses itérateurs.

--- partie2.c
+++ partie2.c
@@ -3,17 +3,22 @@
 #include <stdlib.h>
 #include <math.h>
 
-List* createListFromValue(int n) {
+List* createListFromValue(int n){
+    // numLevels est en réalité numCells (le nombre de cells dans la liste entière)
     int numLevels = pow(2, n) - 1;
     int* levels = (int*)malloc(sizeof(int) * numLevels);
+    // boucle for qui initialise le niveau de toutes les cellules à 1.
     for (int i = 0; i < numLevels; i++) {
         levels[i] = 1;
     }
-
+    // What the hell is Z ??
     int z = 1;
+    // Z va de 1 à nombre maxi de niveaux (de la liste)
     while (z < n) {
+        int v = pow(2, z);
+        //boucle qui pour chaque Z, parcours toutes les cells (Avec j : indice de cellule)
         for (int j = 1; j < numLevels; j++) {
-            int v = pow(2, z);
+            //Si l'indice de notre cellule(j) modulo v(v est une puissance de n) égal 0, alors on rajoute un niveau à notre cellule.
             if ((j %v) == 0) {
                 levels[j-1]++;
             }
