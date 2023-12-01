5cbfd85b0c0f552d6c8c8b672f0e1e7bac883c33
MatALass <115532932+MatALass@users.noreply.github.com>
2023-12-01 19:19:32 +0100

    Update partie3.h

--- partie3.h
+++ partie3.h
@@ -34,39 +34,3 @@ typedef struct Cell {
 typedef struct List {
     Cell cellsHeads[4]; 
 } List;
-
-
-Contact *createContact(char *nom, char *prenom) {
-    Contact *newContact = malloc(sizeof(Contact));
-    newContact->nom = strdup(nom);
-    newContact->prenom = strdup(prenom);
-    return newContact;
-}
-
-
-Appointment *createAppointment(char *date, char *heure, char *duree, char *objet) {
-    Appointment *newAppointment = malloc(sizeof(Appointment));
-    newAppointment->date = strdup(date);
-    newAppointment->heure = strdup(heure);
-    newAppointment->duree = strdup(duree);
-    newAppointment->objet = strdup(objet);
-    return newAppointment;
-}
-
-
-CellLevel0 *createCellLevel0(Contact *contact, Appointment *appointment) {
-    CellLevel0 *newCell = malloc(sizeof(CellLevel0));
-    newCell->contact = contact;
-    newCell->appointment = appointment;
-    newCell->next = NULL;
-    return newCell;
-}
-
-
-Cell *createCell(char letter) {
-    Cell *newCell = malloc(sizeof(Cell));
-    newCell->letter = letter;
-    newCell->next = NULL;
-    newCell->down = NULL;
-    return newCell;
-}
