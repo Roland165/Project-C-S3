eaea03c509b1c86d90a7e4dd9def7e70c5d78d63
Roland165 <115532927+Roland165@users.noreply.github.com>
2023-12-07 22:28:01 +0100

    Update main.c

--- main.c
+++ main.c
@@ -15,31 +15,58 @@ int main() {
     displayCellsInList(myList);*/
 
     //partie 2
-    int nbLevels = 3;
-    List* myList2 = createListFromValue(nbLevels);
+    //int nbLevels = 3;
+    //List* myList2 = createListFromValue(nbLevels);
     //displayCellsInList(myList2);
     //displaySearchFromHighestLevel(myList2, 8);
 
     //printf("debug search low bool = %d\n",searchForNbrValuesFromLowestLevel(myList2, 4, 30));
     //printf("debug search high bool = %d\n",searchForNbrValuesFromHighestLevel(myList2, 4, 30));
 
-    displayTimeBothSearchesForNbrLevelsMaxAndNbrValues(15,1000);
-    displayTimeBothSearchesForNbrLevelsMaxAndNbrValues(15,10000);
-    displayTimeBothSearchesForNbrLevelsMaxAndNbrValues(15,100000);
+    //displayTimeBothSearchesForNbrLevelsMaxAndNbrValues(15,1000);
+    //displayTimeBothSearchesForNbrLevelsMaxAndNbrValues(15,10000);
+    //displayTimeBothSearchesForNbrLevelsMaxAndNbrValues(15,100000);
 
     //partie 3
-    /*
-    char* myStr = scanString();
-    printf("result: %s\n",myStr);
-    char* myStr2 = scanString();
-    printf("result: %s \n",myStr2);
+
+    //char* myStr = scanString();
+    //printf("result: %s\n",myStr);
+    //char* myStr2 = scanString();
+    //printf("result: %s \n",myStr2);
 
     //Contact* myContact = createContact(myStr,myStr2);
     //displayContact(myContact);
 
+
+    LLCRDV *llcrdv =createLLCRDV();
+
     RDV* myRDV = createRDV(01,10,2000,23,59,"Coucou les musulmans moi je mange de la glace");
-    displayRDV(*myRDV);
-     */
+    RDV* secondRDV = createRDV(20,18,2057,14,18,"Ca marche?");
+    //RDV* thirdRDV = createRDV(20,18,2057,14,18,"Ca marche?");
+
+    //addRDVtoLLCRDV(myRDV,llcrdv);
+    //addRDVtoLLCRDV(secondRDV,llcrdv);
+    //addRDVtoLLCRDV(thirdRDV,llcrdv);
+
+    //displayLLCRDV(*llcrdv);
+
+    ListAgenda *listagenda = createEmptyListAgenda();
+    Contact *emmanouel = createContact("Dupont","Emmanouel");
+    Entree *entreeemmanouel= createEntree(*emmanouel);
+    addRDVtoLLCRDV(myRDV,llcrdv);
+    entreeemmanouel->myLLCRDV=*llcrdv;
+    addRDVtoLLCRDV(secondRDV,llcrdv);
+    addEntreetoList(listagenda, entreeemmanouel);
+    Contact *nathan = createContact("Dugrond","Nathan");
+    Entree *entreenathan= createEntree(*nathan);
+    addEntreetoList(listagenda, entreenathan);
+    Contact *roland = createContact("Fontanes","Roland");
+    Entree *entreeroland= createEntree(*roland);
+    addEntreetoList(listagenda, entreeroland);
+    Contact *elia = createContact("Eglantine","Elia");
+    Entree *entreeelia= createEntree(*elia);
+    addEntreetoList(listagenda, entreeelia);
+    displayList(*listagenda);
 
     return 0;
 }
