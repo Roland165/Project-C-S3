488f95eef3128e675175fc80a7afbb8c00827702
MatALass <115532932+MatALass@users.noreply.github.com>
2023-12-08 15:11:39 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,43 +1,31 @@
 #include "partie2.h"
-#include "string.h"
-#include "stdio.h"
 #include "agenda.h"
-
 
 int main() {
     //partie 1
-    /*List* myList = createEmptyList(5);
+    /*
+    List* myList = createEmptyList(5);
     addCellToHeadList(myList, 3, 3);
     addCellToHeadList(myList, 6, 2);
     addCellToHeadList(myList, 9, 3);
     addCellToHeadList(myList, 7, 1);
     addCellToHeadList(myList, 15,1);
-    displayCellsInList(myList);*/
+    displayCellsInList(myList);
 
     //partie 2
-    //int nbLevels = 3;
-    //List* myList2 = createListFromValue(nbLevels);
-    //displayCellsInList(myList2);
-    //displaySearchFromHighestLevel(myList2, 8);
-
-    //printf("debug search low bool = %d\n",searchForNbrValuesFromLowestLevel(myList2, 4, 30));
-    //printf("debug search high bool = %d\n",searchForNbrValuesFromHighestLevel(myList2, 4, 30));
-
-    //displayTimeBothSearchesForNbrLevelsMaxAndNbrValues(15,1000);
-    //displayTimeBothSearchesForNbrLevelsMaxAndNbrValues(15,10000);
-    //displayTimeBothSearchesForNbrLevelsMaxAndNbrValues(15,100000);
-
+    int nbLevels = 15;
+    List* myList2 = createListFromValue(nbLevels);
+    displaySearchInTheLowestLevel(myList2, 9);
+    displaySearchInTheLowestLevel(myList2, 3000);
+    displaySearchInTheLowestLevel(myList2, 17);
+    displaySearchInTheLowestLevel(myList2, 998);
+    displaySearchFromHighestLevel(myList2, 8);
+    displaySearchFromHighestLevel(myList2, 16);
+    displaySearchFromHighestLevel(myList2, 62);
+    displaySearchFromHighestLevel(myList2, 100);
+    displayTimeBothSearchesForNbrLevelsMaxAndNbrValues(14,100000);
+    */
     //partie 3
-
-    //char* myStr = scanString();
-    //printf("result: %s\n",myStr);
-    //char* myStr2 = scanString();
-    //printf("result: %s \n",myStr2);
-
-    //Contact* myContact = createContact(myStr,myStr2);
-    //displayContact(myContact);
-
-
     LLCRDV *llcrdv =createLLCRDV();
 
     RDV* myRDV = createRDV(01,10,2000,23,59,"Coucou les musulmans moi je mange de la glace");
@@ -56,17 +44,19 @@ int main() {
     addRDVtoLLCRDV(myRDV,llcrdv);
     entreeemmanouel->myLLCRDV=*llcrdv;
     addRDVtoLLCRDV(secondRDV,llcrdv);
-    addEntreetoList(listagenda, entreeemmanouel);
+    addEntreetoAllList(listagenda, entreeemmanouel);
     Contact *nathan = createContact("Dugrond","Nathan");
     Entree *entreenathan= createEntree(*nathan);
-    addEntreetoList(listagenda, entreenathan);
+    addEntreetoAllList(listagenda, entreenathan);
     Contact *roland = createContact("Fontanes","Roland");
     Entree *entreeroland= createEntree(*roland);
-    addEntreetoList(listagenda, entreeroland);
+    addEntreetoAllList(listagenda, entreeroland);
     Contact *elia = createContact("Eglantine","Elia");
     Entree *entreeelia= createEntree(*elia);
-    addEntreetoList(listagenda, entreeelia);
+    addEntreetoAllList(listagenda, entreeelia);
     displayList(*listagenda);
-
-    return 0;
+    displayAgendaInList(listagenda);
+    char* try = (char *) "Dupont";
+    displayInformationFromSomeone(try, *listagenda);
+    //menu();
 }
