907ca24d259af6e94cd3a19bf4d6060b42bc6f84
MatALass <115532932+MatALass@users.noreply.github.com>
2023-12-09 14:23:44 +0100

    Update and rename cellandcells.h to cell_and_cells.h

---
+++ cell_and_cells.h
@@ -0,0 +1,19 @@
+#ifndef PROJET_C_S3_MAX_CELLANDCELLS_H
+#define PROJET_C_S3_MAX_CELLANDCELLS_H
+
+
+// Définition d'une structure Cells représentant un tableau de pointeurs vers des Cell
+typedef struct Cells {
+    struct Cell** next;  // Tableau de pointeurs vers les cellules au niveau suivant
+} Cells;
+
+// Définition d'une structure Cell représentant une cellule avec plusieurs niveaux
+typedef struct Cell {
+    int value;           // Valeur stockée dans la cellule
+    Cells cellsNext;     // Structure Cells représentant les niveaux suivants
+    int nbLevels;        // Nombre de niveaux dans la liste à niveaux
+} Cell;
+
+
+Cell* createCell(int, int);
+#endif
