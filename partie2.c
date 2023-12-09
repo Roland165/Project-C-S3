6627ebe55b8163e19230631c2026b6bb167ead9b
MatALass <115532932+MatALass@users.noreply.github.com>
2023-12-09 17:08:11 +0100

    Update partie2.c

--- partie2.c
+++ partie2.c
@@ -1,46 +1,52 @@
 #include "partie2.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include "timer.h"
-
 List *createListFromValue(int n) {
     if (n == 0) {
         return NULL;
     }
-    // numLevels est en réalité numCells (le nombre de cells dans la liste entière)
-    int numLevels = pow(2, n) - 1;
-    int *levels = (int *) malloc(sizeof(int) * numLevels);
-    // boucle for qui initialise le niveau de toutes les cellules à 1.
-    for (int i = 0; i < numLevels; i++) {
-        levels[i] = 1;
+
+    // Calculer le nombre total de niveaux dans la liste, l'utilisation de (1 << n) permet d'éviter un include
+    int totalLevels = (1 << n) - 1;
+
+    // Allouer un tableau pour stocker les niveaux de chaque cellule
+    int *cellLevels = (int *)malloc(sizeof(int) * totalLevels);
+
+    // Initialiser tous les niveaux à 1
+    for (int i = 0; i < totalLevels; i++) {
+        cellLevels[i] = 1;
     }
-    // What the hell is Z ??
-    int z = 1;
-    // Z va de 1 à nombre maxi de niveaux (de la liste)
-    while (z < n) {
-        int v = pow(2, z);
-        //boucle qui pour chaque Z, parcours toutes les cells (Avec j : indice de cellule)
-        for (int j = 1; j < numLevels; j++) {
-            //Si l'indice de notre cellule(j) modulo v(v est une puissance de n) égal 0, alors on rajoute un niveau à notre cellule.
-            if ((j % v) == 0) {
-                levels[j - 1]++;
-            }
+
+    // Boucle pour ajuster les niveaux en fonction de la puissance de 2
+    for (int currentPower = 1; currentPower < n; currentPower++) {
+        int increment = (1 << currentPower);
+        int currentIndex = increment - 1; // Commencer à l'indice correspondant à la puissance de 2
+
+        // Ajuster les niveaux en sautant les cellules de même niveau
+        while (currentIndex < totalLevels) {
+            cellLevels[currentIndex]++;
+            currentIndex += increment;
         }
-        z++;
     }
 
+    // Créer une nouvelle liste vide
     List *newList = createEmptyList(n);
-    for (int i = 0; i < numLevels; i++) {
-        addCellToHeadList(newList, i + 1, levels[i]);
+
+    // Insérer les cellules dans la liste en respectant l'ordre
+    for (int i = 0; i < totalLevels; i++) {
+        insertCellInOrder(newList, i + 1, cellLevels[i] - 1);
     }
 
-    free(levels);
+    // Libérer la mémoire allouée pour le tableau des niveaux
+    free(cellLevels);
 
+    // Retourner la nouvelle liste créée
     return newList;
 }
 
-Cell *searchInTheLowestLevel(List *myList, int target) {
+// Recherche d'une valeur dans le niveau le plus bas de la liste
+Cell *searchInLowestLevel(List *myList, int target) {
     Cell *current = myList->cellsHeads.next[0];
 
     while (current != NULL) {
@@ -53,34 +59,44 @@ Cell *searchInTheLowestLevel(List *myList, int target) {
     return NULL;
 }
 
-Cell* searchFromHighestLevel(List* myList, int target) {
+// Recherche d'une valeur à partir du niveau le plus élevé
+Cell *searchFromHighestLevel(List *myList, int target) {
     int highestLevel = myList->nbLevels - 1;
     Cell *current = myList->cellsHeads.next[highestLevel];
-    if(target < myList->cellsHeads.next[0]->value){
+
+    // Si la cible est inférieure à la plus petite valeur dans le niveau le plus bas, la cible ne peut pas être dans la liste
+    if (target < myList->cellsHeads.next[0]->value) {
         return NULL;
     }
-    while (current->value > target){
+
+    while (current->value > target) {
         highestLevel = highestLevel - 1;
         current = myList->cellsHeads.next[highestLevel];
     }
+
     if (current->value == target) {
         return current;
     }
-    while (highestLevel >= 0){
+
+    while (highestLevel >= 0) {
         if (current->cellsNext.next[highestLevel] != NULL && current->cellsNext.next[highestLevel]->value == target) {
             return current->cellsNext.next[highestLevel];
         }
+
         if (current->cellsNext.next[highestLevel] == NULL || current->cellsNext.next[highestLevel]->value > target) {
             highestLevel--;
         } else {
             current = current->cellsNext.next[highestLevel];
         }
     }
+
     return NULL;
 }
 
+// Affichage du résultat de la recherche à partir du niveau le plus élevé
 void displaySearchFromHighestLevel(List *myList, int target) {
     Cell *resultHighestLevel = searchFromHighestLevel(myList, target);
+
     if (resultHighestLevel != NULL) {
         printf("Found %d at level %d.\n", target, resultHighestLevel->nbLevels - 1);
     } else {
@@ -88,80 +104,72 @@ void displaySearchFromHighestLevel(List *myList, int target) {
     }
 }
 
-void displaySearchInTheLowestLevel(List *mylist, int target) {
-    Cell *resultLevel0 = searchInTheLowestLevel(mylist, target);
+// Affichage du résultat de la recherche dans le niveau le plus bas
+void displaySearchInLowestLevel(List *myList, int target) {
+    Cell *resultLevel0 = searchInLowestLevel(myList, target);
 
     if (resultLevel0 != NULL) {
         printf("Found %d at level 0.\n", target);
     } else {
         printf("%d not found at level 0.\n", target);
     }
+
 }
 
-
-int searchForNbrValuesFromLowestLevel(List *myList, int nbLevels, int nbrValues) {
+// Recherche de plusieurs valeurs à partir du niveau le plus bas
+int searchValuesInLowestLevel(List *myList, int nbLevels, int nbrValues) {
     if ((nbrValues != 0) && (myList != NULL)) {
-        //initialisation de la seed de random
         srand(31);
-        //valeur maxi qu'on doit chercher dans la liste (pour le random)
-        int maxiValueInList = (pow(2, nbLevels)) - 1;
-        int iterNbrValues = 1;
-        while (iterNbrValues <= nbrValues) {
-            int valueToSearch = rand() % (maxiValueInList+1);
-            //printf("DEBUG: random valueToSearch = %d\n", valueToSearch);
-            searchInTheLowestLevel(myList, valueToSearch);
-            iterNbrValues++;
+        int maxiValueInList = (1 << nbLevels) - 1;
+
+        for (int iterNbrValues = 1; iterNbrValues <= nbrValues; iterNbrValues++) {
+            int valueToSearch = rand() % (maxiValueInList + 1);
+            searchInLowestLevel(myList, valueToSearch);
         }
         return 1;
     }
     return 0;
 }
 
-
-int searchForNbrValuesFromHighestLevel(List *myList, int nbLevels, int nbrValues) {
+// Recherche de plusieurs valeurs à partir du niveau le plus élevé
+int searchValuesFromHighestLevel(List *myList, int nbLevels, int nbrValues) {
     if ((nbrValues != 0) && (myList != NULL)) {
-        //initialisation de la seed de random
         srand(31);
-        //valeur maxi qu'on doit chercher dans la liste (pour le random)
-        int maxiValueInList = (pow(2, nbLevels)) - 1;
-        int iterNbrValues = 1;
-        while (iterNbrValues <= nbrValues) {
-            int valueToSearch = rand() % (maxiValueInList+1);
-            //printf("DEBUG: random valueToSearch = %d ", valueToSearch);
+        int maxiValueInList = (1 << nbLevels) - 1;
+
+        for (int iterNbrValues = 1; iterNbrValues <= nbrValues; iterNbrValues++) {
+            int valueToSearch = rand() % (maxiValueInList + 1);
             searchFromHighestLevel(myList, valueToSearch);
-            //printf("FOUND.\n");
-            iterNbrValues++;
         }
         return 1;
     }
     return 0;
 }
 
-
-void displayTimeBothSearchesForOneLevelAndNbrValues(int level, int nbrValues){
-    List* myList = createListFromValue(level);
-    startTimer();
-    if((searchForNbrValuesFromLowestLevel(myList, level, nbrValues))==1){
-        stopTimer();
-        printf("Lowest Level Seach: nbrValues=%d; level=%d; ",nbrValues,level);
-        printf("%s",getTimeAsString());
-        printf("\n");
+// Affichage des temps de recherche à partir des niveaux les plus bas et les plus élevés
+void displayTimeBothSearches(int maxLevels, int nbrValues) {
+    for (int nbrLevels = 7; nbrLevels <= maxLevels; nbrLevels++) {
+        displayTimeBothSearchesForOneLevel(nbrLevels, nbrValues);
     }
-    startTimer();
-    if((searchForNbrValuesFromHighestLevel(myList, level, nbrValues))==1){
-        stopTimer();
-        printf("Highest Level Seach: nbrValues=%d; level=%d; ",nbrValues,level);
-        printf("%s",getTimeAsString());
-        printf("\n");
-    }
-    //printf("Both Searches for nbrValues=%d; level=%d DONE.\n",nbrValues,level);
 }
 
+// Affichage des temps de recherche à partir d'un niveau spécifique pour un nombre donné de valeurs
+void displayTimeBothSearchesForOneLevel(int level, int nbrValues) {
+    List *myList = createListFromValue(level);
 
+    startTimer();
+    if ((searchValuesInLowestLevel(myList, level, nbrValues)) == 1) {
+        stopTimer();
+        printf("Lowest Level Search: nbrValues=%d; level=%d; ", nbrValues, level);
+        printf("%s", getTimeAsString());
+        printf("\n");
+    }
 
-void displayTimeBothSearchesForNbrLevelsMaxAndNbrValues(int maxLevels, int nbrValues){
-    // MIN LEVEL IS 8
-    for(int nbrLevels = 8; nbrLevels <= maxLevels; nbrLevels++){
-        displayTimeBothSearchesForOneLevelAndNbrValues(nbrLevels, nbrValues);
+    startTimer();
+    if ((searchValuesFromHighestLevel(myList, level, nbrValues)) == 1) {
+        stopTimer();
+        printf("Highest Level Search: nbrValues=%d; level=%d; ", nbrValues, level);
+        printf("%s", getTimeAsString());
+        printf("\n");
     }
 }
