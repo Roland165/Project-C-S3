c5967dfd798db082dc5c53c0f0933f5454a58baa
MatALass <115532932+MatALass@users.noreply.github.com>
2023-12-09 15:33:17 +0100

    Update list.c

--- list.c
+++ list.c
@@ -12,10 +12,10 @@ List* createEmptyList(int nbLevelsMaxi) {
     newList->nbLevels = nbLevelsMaxi;
 
     // Allocation dynamique du tableau de pointeurs pour les niveaux de la liste
-    newList->cellsHeads.next = (Cell**)malloc(sizeof(Cell*) * nbLevelsMaxi);
+    newList->cellsHeads.next = (Cell**)malloc(sizeof(Cell*) * nbLevelsMaxi+1);
 
     // Initialisation de chaque pointeur à NULL
-    for (int i = 0; i < nbLevelsMaxi; i++) {
+    for (int i = 0; i < nbLevelsMaxi+1; i++) {
         newList->cellsHeads.next[i] = NULL;
     }
 
@@ -23,7 +23,7 @@ List* createEmptyList(int nbLevelsMaxi) {
 }
 
 // Ajout d'une cellule avec la valeur spécifiée à la tête de la liste
-List* addCellToHeadList(List* myList, int value, int nbLevels) {
+List* insertCellInOrder(List* myList, int value, int nbLevels) {
     Cell* newCell = createCell(value, nbLevels);
 
     // Boucle pour parcourir les niveaux et insérer la nouvelle cellule
@@ -40,24 +40,24 @@ List* addCellToHeadList(List* myList, int value, int nbLevels) {
 }
 
 
-// Affichage des cellules d'un niveau spécifié dans la liste
-void displayCellsInListByLevel(List* myList, int level) {
+// Affichage de l'ensemble des cellules de la liste pour un niveau donné
+void displayCellsForLevel(List* myList, int level) {
     if (myList->cellsHeads.next[0] == NULL) {
-        printf("This list is empty.\n");
+        printf("\nThis list is empty.\n");
         return;
     }
 
-    if (level < 0 || level >= myList->nbLevels) {
-        printf("Error: Cannot display this level. The level of a list starts at 0 and is less than or equal to the total number of levels.\n");
+    if (level < 0 || level > myList->nbLevels) {
+        printf("\nError: Cannot display this level. The level of a list starts at 0 and is less than or equal to the total number of levels.\n");
         return;
     }
 
-    Cell* temp = myList->cellsHeads.next[level];
-    printf("Display of list by level %d : ", level);
+    Cell* temp = myList->cellsHeads.next[level-1];
+    printf("\nDisplay of list by level %d : ", level);
 
     while (temp != NULL) {
         printf("%d -> ", temp->value);
-        temp = temp->cellsNext.next[level];
+        temp = temp->cellsNext.next[level-1];
     }
 
     printf("NULL\n");
@@ -65,16 +65,16 @@ void displayCellsInListByLevel(List* myList, int level) {
 
 
 
-// Affichage de toutes les cellules dans la liste, par niveau
-void displayCellsInList(List* myList) {
+// Affichage de tous les niveaux de la liste
+void displayAllLevels(List* myList) {
     if (myList == NULL) {
-        printf("DisplayCellsInList error: List is empty (NULL).\n");
+        printf("List is empty (NULL).\n");
         return;
     }
 
-    for (int level = 1; level <= myList->nbLevels; level++) {
+    for (int level = 0; level <= myList->nbLevels; level++) {
         Cell* temp = myList->cellsHeads.next[0];
-        printf("[list head_%d @-]--", level - 1);
+        printf("[list head_%d @-]--", level);
 
         while (temp != NULL) {
             // Affichage de la cellule si elle a un niveau suffisant
