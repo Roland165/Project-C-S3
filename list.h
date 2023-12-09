38099e77dc8ebaeadc3068ebd107b2adbdf78252
MatALass <115532932+MatALass@users.noreply.github.com>
2023-12-09 15:36:12 +0100

    Update list.h

--- list.h
+++ list.h
@@ -3,15 +3,22 @@
 
 #include "cell_and_cells.h"
 
-typedef struct List{
-    Cells cellsHeads;
-    int nbLevels;
-}List;
+// Définition de la structure List représentant une liste
+typedef struct List {
+    Cells cellsHeads;  // Structure de cellules représentant les têtes de chaque niveau de la liste
+    int nbLevels;       // Nombre total de niveaux dans la liste
+} List;
 
+// Fonction pour créer une liste vide avec un certain nombre de niveaux
 List* createEmptyList(int);
-void displayCellsForLevel(List* myList, int level);
-List* insertCellInOrder(List*, int, int);
-void displayAllLevels(List*);
 
+// Fonction pour afficher les cellules pour un niveau spécifique de la liste
+void displayCellsForLevel(List*, int);
+
+// Fonction pour insérer une cellule dans la liste de manière ordonnée
+List* insertCellInOrder(List*, int, int);
+
+// Fonction pour afficher toutes les cellules de tous les niveaux de la liste
+void displayAllLevels(List*);
 
 #endif
